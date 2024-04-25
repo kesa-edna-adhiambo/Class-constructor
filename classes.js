1259b4b0045ff217500d830a39474522f9fee244
kesa-edna-adhiambo <edna@gmail.com>
2024-04-25 07:11:29 +0300

    add class file

---
+++ classes.js
@@ -0,0 +1,20 @@
+//QUESTUION 1   
+class Car {
+    constructor(make,model,year,isAvailable){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.isAvailable = isAvailable
+    }
+    toggleAvailability(){
+        this.isAvailable=!isAvailable
+    }
+}
+    const car1= new Car('mazda','Demio',2020,true);
+    console.log(car1.isAvailable);
+
+class Rental {
+    constructor(car,rentorName,)
+
+}
+
